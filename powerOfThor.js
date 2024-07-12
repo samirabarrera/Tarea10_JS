a35edf4510f871ab6b0d18ef23fbe1611618550c
Samira Barrera <samira.barrera@galileo.edu>
2024-07-11 21:46:43 -0600

    corrigiendo el código

--- powerOfThor.js
+++ powerOfThor.js
@@ -11,32 +11,32 @@ const lightY = parseInt(inputs[1]); // the Y position of the light of power
 const initialTx = parseInt(inputs[4]); // Thor's starting X position
 const initialTy = parseInt(inputs[2]); // Thor's starting Y position
 
-let thorX = "initialTX";
-let thorY = "initialTY";
+var thorX = initialTX;
+var thorY = initialTY;
 
 // game loop
 while (true) {
-    let remainingTurns = parseInt(readline());
-    let directionX = "";
-    let directionY = "";
-    
-    if (thorY > lightY) {
-        directionY = "N";
-        thorY === thorY - 1;
-    } else if (thorY < lightY) {
-        directionY = "S";
-        thorY === thorY +1;
-    }
+    var remainingTurns = parseInt(readline());
 
+    var directionX = "";
+    var directionY = "";
+    
     if (thorX > lightX) {
         directionX = "W";
-        thorX === thorX -1;
-    } else if (thorX < lightX){
-        directionY = "E";
-        thorX === thorX +1;
-    }    
+        thorX--;
+    } else if (thorX < lightX) {
+        directionX = "E";
+        thorX++;
+    }
 
+    if (thorY > lightY) {
+        directionY = "N";
+        thorY--;
+    } else if (thorY < lightY){
+        directionY = "S";
+        thorY++;
+    }    
+}
     // Write an action using print()
     // To debug: printErr('Debug messages...');
-    console.log(directionY + directionX); 
-}
+    print(directionY + directionX); // A single line providing the move to be made: N NE 
